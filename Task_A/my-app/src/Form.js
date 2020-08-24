63e94dc2c739680f1dd6066f547b7b2df8da997c
hoholyin <holyin97@gmail.com>
2020-08-24 20:51:33 +0800

    Add working nginx connection

--- Task_A/my-app/src/Form.js
+++ Task_A/my-app/src/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './App.css';
 
 class Form extends React.Component {
@@ -7,14 +8,27 @@ class Form extends React.Component {
         this.state = {arg: ""}
     }
     handleChange(e) {
-        this.setState({arg: e});
+        this.setState({arg: e.target.value});
         console.log('handle change: ' + e.target.value);
+    }
+    submitRequest = () => {
+        console.log('submitting request');
+        var xhr = new XMLHttpRequest()
+        xhr.addEventListener('load', () => {
+            console.log(xhr.responseText);
+        })
+        xhr.open('GET', 'http://localhost:80/api/refs?data=' + this.state.arg);
+        xhr.send()
     }
     render() {
         return (
-            <input type="text" id="input" onChange={(e) => this.handleChange(e)}></input>
+            <div id="input">
+                <div id="title">Inspect and open the network tab to see the GET request</div>
+                <input id="blank" type="text" placeholder='Type something...' onChange={(e) => this.handleChange(e)}></input>
+                <button id="submit" onClick={this.submitRequest} type="button">Send GET request</button>
+            </div>
         )
     }
 }
 
-export default Form
+export default withRouter(Form)
